5d2ceb585fe0e237d598b202b91d2171727fd2a1
feraistanti <fistanti551@gmail.com>
2025-10-23 11:27:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/questadvancelayout_083/ui/theme/UIPertama.kt
@@ -0,0 +1,9 @@
+package com.example.questadvancelayout_083.ui.theme
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ActifitasPertama(modifier: Modifier){
+    Column(modifier =)
+}
+

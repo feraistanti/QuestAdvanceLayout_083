ab5be05c870d650a452c5ecc5ab40a4652fbd43a
feraistanti <fistanti551@gmail.com>
2025-10-23 11:53:45 +0700

    Menambahkan komponen Text, Spacer, dan Card dengan Row pada ActifitasPertama

--- app/src/main/java/com/example/questadvancelayout_083/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_083/ui/theme/UIPertama.kt
@@ -1,18 +1,42 @@
 package com.example.questadvancelayout_083.ui.theme
 
+import android.R
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun ActifitasPertama(modifier: Modifier){
     Column(modifier = Modifier.padding(top=100.dp)
-        .filMaxSize(),
-        horizontalAlignment = Alighment.CenterHorizontally
+        .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text( text = stringResource( id = R.string.prodi),
+        Text(stringResource( R.string.prodi),
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold)
+        Text( stringResource( R.string.univ),
+            fontSize = 22.sp)
+        Spacer(modifier = Modifier.height( 25.dp))
+        Card (modifier = Modifier)
+            .fillMaxWidth( 1f)
+            .padding( all = 12.dp),
+            colors = CardDefaults.cardColors(
+                ContainerColor = Color.DarkGray
+            )) {
+                Row() {
+
+                }
+    }
     }
 }

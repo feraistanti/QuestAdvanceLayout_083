8b6569842f9da5242d93b039105ac323c799388b
feraistanti <fistanti551@gmail.com>
2025-10-23 11:55:55 +0700

    Menambahkan komponen Image dengan resource logo_umy pada ActifitasPertama

--- app/src/main/java/com/example/questadvancelayout_083/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_083/ui/theme/UIPertama.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -35,7 +36,10 @@ fun ActifitasPertama(modifier: Modifier){
                 ContainerColor = Color.DarkGray
             )) {
                 Row() {
-
+                    val gambar = painterResource( R.drawable.logo_umy)
+                    image(
+                        painter = gambar,
+                    )
                 }
     }
     }

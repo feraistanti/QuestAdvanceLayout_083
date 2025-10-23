233a152b67ec194aec7e427337ad6cd0765b9ae5
feraistanti <fistanti551@gmail.com>
2025-10-23 11:35:43 +0700

    Menyelesaikan layout Column pada ActifitasPertama dengan Text dan alignment

--- app/src/main/java/com/example/questadvancelayout_083/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_083/ui/theme/UIPertama.kt
@@ -1,8 +1,18 @@
 package com.example.questadvancelayout_083.ui.theme
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 
 @Composable
 fun ActifitasPertama(modifier: Modifier){
-    Column(modifier =)
+    Column(modifier = Modifier.padding(top=100.dp)
+        .filMaxSize(),
+        horizontalAlignment = Alighment.CenterHorizontally
+    ) {
+        Text( text = stringResource( id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold)
+    }
 }

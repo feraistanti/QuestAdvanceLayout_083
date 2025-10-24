e0af6120ce9658ea57217e9c30a7763f00c4bbb7
feraistanti <fistanti551@gmail.com>
2025-10-24 08:01:56 +0700

    Nambah warna hijau pada Card di ActifitasPertama

--- app/src/main/java/com/example/questadvancelayout_083/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_083/ui/theme/UIPertama.kt
@@ -126,3 +126,9 @@ fun ActifitasPertama(modifier: Modifier){
     Card (modifier = Modifier
         .fillMaxWidth( 1f)
         .padding( all = 12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Green
+        )) {
+
+
+
